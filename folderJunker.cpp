23e1b69c1c0743a942045c9076b4a0790e692492
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2023-04-25 10:19:49 +0600

    Add/Enhance the prompts and a help menu to the program Fixes #1
    - Formatted the folderJunker.cpp
    - Modified some promts
    - Enhanced the previously built help menu
    
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>

--- folderJunker.cpp
+++ folderJunker.cpp
@@ -1,14 +1,13 @@
 /*
-A pet project of Mushfiqur Rahman Abir under GPL 3.0 License. The intention of this tool
-will be to create many dummy/junk folders in just one click. The usage of this tool will be of course for test purposes.
-Developed By: Mushfiqur Rahman Abir
-Year: 2021
-Build System: CMake
+A pet project of Mushfiqur Rahman Abir under GPL 3.0 License. The intention of
+this tool will be to create many dummy/junk folders in just one click. The usage
+of this tool will be of course for test purposes. Developed By: Mushfiqur Rahman
+Abir Year: 2021 Build System: CMake
 */
 
 // Includes
-#include <iostream>
 #include <cstring>
+#include <iostream>
 
 // Personal includes
 #include "fjhome.hpp"
@@ -23,58 +22,55 @@ Build System: CMake
 #define UNKNOWN_ERR 103
 
 // Driver Function
-int main(int argc, char const *argv[])
-{
-	if (argc <= 1) /* If no args passed */
-		std::cout << "No args passed. Specify the numbers of folder you want to create & try again";
-	else /* If args are passed */
-	{
-		FJ::FolderJunker *fj = new FJ::FolderJunker();
-		fj->initializeTitle();
+int main(int argc, char const *argv[]) {
+  if (argc <= 1) /* If no args passed */
+    std::cout
+        << "No arguments passed. Specify the numbers of folder you want to "
+           "create & try again. Example usage: folderJunker 100\n\nSee "
+           "folderJunker --help for more info.";
+  else /* If args are passed */
+  {
+    FJ::FolderJunker *fj = new FJ::FolderJunker();
+    fj->initializeTitle();
 
-		if (COMP(argv[1], "-v") || COMP(argv[1], "--version"))
-			std::cout << "Current App version: " << FJ::currentVersion;
-		else if (COMP(argv[1], "-h") || COMP(argv[1], "--help"))
-		{
-			FJ::Help help;
-			help.listAvailableCommands();
-		}
-		else /* For handling  positive int args */
-		{
-			try
-			{
-				int val = std::stoi(argv[1]);
+    if (COMP(argv[1], "-v") || COMP(argv[1], "--version"))
+      std::cout << "Current App version: " << FJ::currentVersion;
+    else if (COMP(argv[1], "-h") || COMP(argv[1], "--help")) {
+      FJ::Help help;
+      help.listAvailableCommands();
+    } else /* For handling  positive int args */
+    {
+      try {
+        int val = std::stoi(argv[1]);
 
-				bool num_accepted = false;
-				try
-				{
-					if (val < 1)
-						throw NEGATIVE_NUMBER;
-					else
-						num_accepted = true;
-				}
-				catch (int errorno)
-				{
-					std::cout << "Negative numbers are not accepted. Error no: " << errorno;
-					std::cout << std::endl;
-				}
+        bool num_accepted = false;
+        try {
+          if (val < 1)
+            throw NEGATIVE_NUMBER;
+          else
+            num_accepted = true;
+        } catch (int errorno) {
+          std::cout << "Negative numbers are not accepted. Error no: "
+                    << errorno;
+          std::cout << std::endl;
+        }
 
-				int &number = val;
-				if (num_accepted)
-					fj->createFolders(number);
-			}
-			catch (std::invalid_argument)
-			{
-				std::cerr << "Invalid argument passed. Please pass positive numbers. Error no " << INVALID_ARG << std::endl;
-			}
-			catch (...)
-			{
-				std::cerr << "Erros occured ! Please try again. Error no " << UNKNOWN_ERR << std::endl;
-			}
-		}
-		// Delete the fj object
-		delete fj;
-	}
+        int &number = val;
+        if (num_accepted)
+          fj->createFolders(number);
+      } catch (std::invalid_argument) {
+        std::cerr << "Invalid argument passed. Please provide positive numbers "
+                     "as arguments. "
+                     "Error no "
+                  << INVALID_ARG << std::endl;
+      } catch (...) {
+        std::cerr << "Errors occured ! Please try again. Error no "
+                  << UNKNOWN_ERR << std::endl;
+      }
+    }
+    // Delete the fj object
+    delete fj;
+  }
 
-	return 0;
+  return 0;
 }
